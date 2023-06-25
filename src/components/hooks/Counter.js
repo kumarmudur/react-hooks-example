c8ece73f8910918b90f7e1743e7aeb25ae23ab9c
kumarmudur <kumarmudur10@gmail.com>
2023-06-25 14:45:42 -0400

    [React-Hooks] react hook counter example

---
+++ src/components/hooks/Counter.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const incrementCount = () => setCount(count + 1);
+    
+    return (
+        <div>
+            <h1>Count - { count }</h1>
+            <button onClick={incrementCount}>Count</button>
+        </div>
+    );
+};
+
+export default Counter;
