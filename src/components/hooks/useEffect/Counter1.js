f9cdc07cd5416e875556a82391c1210f0f9e4df2
kumarmudur <kumarmudur10@gmail.com>
2023-06-29 06:53:36 -0400

    [React-Hooks] useEffect conditional example changes

--- src/components/hooks/useEffect/Counter1.js
+++ src/components/hooks/useEffect/Counter1.js
@@ -2,15 +2,20 @@ import React, { useEffect, useState } from 'react';
 
 const Counter1 = () => {
     const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
 
     const increment = () => setCount(count + 1);
 
+    const onChangeName = (event) => setName(event.target.value);
+
     useEffect(() => {
+        console.log('useEffect - updating document title');
         document.title = `You clicked ${count} times`;
     }, [count]);
 
     return (
         <div>
+            <input type='text' value={name} onChange={onChangeName} />
             <h1>{count} times</h1>
             <button onClick={increment}>Click</button>
         </div>
