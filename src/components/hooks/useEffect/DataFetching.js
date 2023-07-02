501231a7b85f056656b05c3fa8de5b865fdbdc80
kumarmudur <kumarmudur10@gmail.com>
2023-07-02 11:35:47 -0400

    [React-Hooks] data fetching using id example

--- src/components/hooks/useEffect/DataFetching.js
+++ src/components/hooks/useEffect/DataFetching.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 
 const DataFetching = () => {
     const [posts, setPosts] = useState([]);
+    const [id, setId] = useState(1);
+    const [post, setPost] = useState({});
 
     const getPosts = useCallback(async () => {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-        console.log('posts', posts);
-        setPosts(response.data);
-    }, [posts]);
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        setPost(response.data);
+    }, [id]);
 
     useEffect(() => {
         // axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -22,13 +23,17 @@ const DataFetching = () => {
         getPosts();
     }, [getPosts]);
 
+    const onChange = (event) => setId(event.target.value);
+
     return (
         <div>
-            <ul>
+            <input type='text' onChange={onChange} value={id}/>
+            <div>{post.title}</div>
+            {/* <ul>
                 {
                     posts.map(post => <li key={post.id}>{post.title}</li>)
                 }
-            </ul>
+            </ul> */}
         </div>
     );
 };
