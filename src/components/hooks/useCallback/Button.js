5ec2b7176d49acfd195d0d92968c69a2d5e3030a
kumarmudur <kumarmudur10@gmail.com>
2023-07-19 21:21:32 -0400

    [React-Hooks] count component logic changes

---
+++ src/components/hooks/useCallback/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Button = ({ children, handleClick}) => {
+    console.log('Rendering Button ', children);
+    return (
+        <button>
+            
+        </button>
+    ); 
+};
+
+export default Button;
